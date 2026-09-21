6d6cc0c26e7b83a0a49a2248d30ed27bf683837e
amihub <amihub2026@gmail.com>
2026-09-21 10:09:27 +0530

    use

---
+++ src/app/services/user-auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+
+export interface GoogleLoginResponse {
+  success: boolean;
+  message: string;
+  token: string;
+
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    picture: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+
+  private readonly http = inject(HttpClient);
+  getCurrentUser(): Observable<CurrentUserResponse> {
+  return this.http.get<CurrentUserResponse>(
+    `${environment.apiUrl}/user-auth/me`,
+    {
+      withCredentials: true
+    }
+  );
+}
+ googleLogin(
+  credential: string
+): Observable<GoogleLoginResponse> {
+  return this.http.post<GoogleLoginResponse>(
+    `${environment.apiUrl}/user-auth/google`,
+    {
+      credential
+    },
+    {
+      withCredentials: true
+    }
+  );
+}
+  
+}
+export interface CurrentUserResponse {
+  success: boolean;
+
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    picture: string;
+    provider: string;
+    lastLoginAt: string | null;
+  };
+}
